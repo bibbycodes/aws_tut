37f999645782f0ed13ab0f15790d2b9024fa47bd
Robert <bibbycodes@gmail.com>
2020-02-01 18:11:45 +0000

    tina

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+
+app.use(express.static('public'))
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
